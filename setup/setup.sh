b7fd48771be878d89540459058a9da2b82a02248
NiallBunting <niall.bunting@bjss.com>
2019-10-31 12:02:18 +0000

    Basic setup file

---
+++ setup/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Dist upgrade
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+# Create user
+adduser nib
+# Add to sudoers
+usermod -aG sudo nib
+
+# Set this in the sshd settings
+echo "PermitRootLogin no"
+service sshd restart
+
+# Install some basic things
+sudo apt-get update
+
+#Curl Tmux
+sudo apt install curl tmux git pass
+
+#Docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+
+# Change to user
+su nib
+
+# Add keys for easylogin
+mkdir ~/.ssh
+curl https://github.com/NiallBunting.keys >> ~/.ssh/authorized_keys
+
+# Install dot files
+mkdir me
+cd me
+git clone https://github.com/NiallBunting/dotfiles.git
+cd ~
+ln -s ~/me/dotfiles/.bashrc .
+ln -s ~/me/dotfiles/.bash_profile .
+ln -s me/dotfiles/.tmux.conf .
+ln -s me/dotfiles/.vimrc .
+ln -s me/dotfiles/.vim/ .
+# These probably will need some tweaking.
+
+# Pass setup
+pass git init
+pass git remote add origin user@server:~/.password-store
+gpg --import pubkey.asc
+gpg --allow-secret-key-import --import privkey.asc
+
+# Set up firewall
+sudo ufw allow ssh
+sudo ufw enable
